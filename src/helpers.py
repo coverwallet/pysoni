4404203bf654be49ee8d1b906758781dd9d8a59f
Alex Gascon <alexgascon.93@gmail.com>
2018-06-20 10:09:07 +0200

    Fixing CodeClimate issues

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,8 @@
 import random
 
+
 def validate_types(subject, expected_types=[], contained_types=[]):
-    if expected_types and type(subject) not in expected_types:
+    if (expected_types) and (type(subject) not in expected_types):
         raise TypeError('The type of the subject does not match the expected ones')
 
     element = random.choice(subject)

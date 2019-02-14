61deffdb0e1b941756e919018efdfb4c39178bf5
Sergio <sergioelola@hotmail.com>
2019-02-14 11:16:10 +0100

    New test added: postgres_statement method

---
+++ tests/core/test_postgre_statement.py
@@ -0,0 +1,11 @@
+import os
+import pytest
+import psycopg2
+from dotenv import load_dotenv,find_dotenv
+
+load_dotenv(find_dotenv())
+
+def test_statement_integrity(pysoni_client_connection_with_envvars):
+
+    assert pysoni_client_connection_with_envvars.postgre_statement("select 1")
+    assert 1 == 2

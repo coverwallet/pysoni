87d29b534cc44c1d45c9e9428bbfc5b0969953b4
Sergio <sergioelola@hotmail.com>
2019-02-19 14:57:24 +0100

    Small change in the first test in order to check that the name of columns are returned correctly

--- tests/core/test_execute_query.py
+++ tests/core/test_execute_query.py
@@ -7,8 +7,8 @@ load_dotenv(find_dotenv())
 
 def test_execute_query(pysoni_client_connection_with_envvars):
     
-    expected_results = {'results':[(1, 2, 3)],'keys':['?column?','?column?','?column?']}
-    results = pysoni_client_connection_with_envvars.execute_query(queryname= "SELECT 1,2,3")
+    expected_results = {'results':[(1, 2)],'keys':['column1','?column?']}
+    results = pysoni_client_connection_with_envvars.execute_query(queryname = "SELECT 1 AS column1, 2")
     
     assert expected_results == results
 
@@ -17,7 +17,7 @@ def test_execute_query_with_script(pysoni_client_connection_with_envvars):
     path = 'tests/fixtures/'
 
     expected_results = {'results':[(1, 2, 3)],'keys':['?column?','?column?','?column?']}
-    results_with_script = pysoni_client_connection_with_envvars.execute_query(queryname=filename,
+    results_with_script = pysoni_client_connection_with_envvars.execute_query(queryname = filename,
                             path_sql_script=path)
 
     assert expected_results == results_with_script
@@ -26,7 +26,7 @@ def test_execute_query_with_types(pysoni_client_connection_with_envvars):
         
     expected_results = {'results':[(1, 2, 3)],'keys':['?column?','?column?','?column?'],
                             'types':['int4', 'int4', 'int4']}
-    results_with_types = pysoni_client_connection_with_envvars.execute_query(queryname= "SELECT 1,2,3",
+    results_with_types = pysoni_client_connection_with_envvars.execute_query(queryname = "SELECT 1,2,3",
                             types = True)
 
     assert expected_results == results_with_types

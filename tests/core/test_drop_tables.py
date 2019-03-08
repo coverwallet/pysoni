b4bc72377a1f53881158cf034ca234792e62ac98
SergioEG <sergioelola@hotmail.com>
2019-03-08 13:27:18 +0100

    Updated the second test method of 'drop_tables'

--- tests/core/test_drop_tables.py
+++ tests/core/test_drop_tables.py
@@ -23,15 +23,16 @@ def test_drop_tables_with_batch_false_executes_multiple_statements(pysoni_client
     mocker.patch.object(pysoni_client, "connection")
     mocker.patch.object(pysoni_client, "postgre_statement")
     
-    expected_statement = "DROP TABLE table1"
+    table_to_drop = 'table1'
+    expected_statement = f"DROP TABLE {table_to_drop}"
 
     pysoni_client.drop_tables(
-      table_names=['table1'],
+      table_names=[table_to_drop],
       wait_time=0,
       batch=False
     )
 
     pysoni_client.postgre_statement.assert_called_once_with(
-        expected_statement,
-        timesleep=0
+      expected_statement,
+      timesleep=0
     )

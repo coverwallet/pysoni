ffc64b4ac8db081affa87d566d178632e31e9fb5
Alex Gascón <alex.gascon@coverwallet.com>
2019-03-08 13:05:26 +0100

    Style fixes

--- tests/core/test_drop_tables.py
+++ tests/core/test_drop_tables.py
@@ -7,11 +7,15 @@ def test_drop_tables_executes_a_single_statement(pysoni_client, mocker):
 
     expected_statement = "DROP TABLES table1, table2, table3"
 
-    pysoni_client.drop_tables(table_names=['table1', 'table2', 'table3'],
-                              wait_time=0, batch=True)
+    pysoni_client.drop_tables(
+      table_names=['table1', 'table2', 'table3'],
+      wait_time=0, batch=True
+    )
+
     pysoni_client.postgre_statement.assert_called_once_with(
-                                expected_statement,
-                                timesleep=0)
+      expected_statement,
+      timesleep=0
+    )
 
 
 def test_drop_tables_with_batch_false_executes_multiple_statements(pysoni_client, 

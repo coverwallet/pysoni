6a616d08b71205c31234364f8eaffb5e0100c514
Javier162380 <javierllorente16@gmail.com>
2019-03-11 13:00:11 +0100

    chaneg test and mock properly

--- tests/core/test_postgre_statement.py
+++ tests/core/test_postgre_statement.py
@@ -2,16 +2,13 @@ import pytest
 import psycopg2
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
 
-def test_postgre_multiple_statement_with_psycopg2_api(pysoni_client_connection_with_envvars, mocker):
-
+def test_postgre_statement_with_psycopg2_api(pysoni_client, mocker):
+    test_postgre_statement_with_psycopg2_api
     expected_result = "CREATE TABLE temp_testing (cover text)"
 
-    pysoni_client_connection_with_envvars.postgre_statement(
-        "CREATE TABLE temp_testing (cover text)")
 
     with mocker.patch.object(psycopg2, 'connect') as mock_connection:
-        mock_connection.cursor.return_value.execute.called_with(
-            expected_result
-        )
+        pysoni_client.postgre_statement(
+            "CREATE TABLE temp_testing (cover text)")
+        mock_connection.cursor.return_value.execute.called_with(expected_result)

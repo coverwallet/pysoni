3254e1bb33afcf14eb3f21b618d377d55762aad6
Javier162380 <javierllorente16@gmail.com>
2019-03-11 13:05:44 +0100

    solve issue in variable

--- tests/core/test_postgre_statement.py
+++ tests/core/test_postgre_statement.py
@@ -7,7 +7,6 @@ def test_postgre_statement_with_psycopg2_api(pysoni_client, mocker):
     test_postgre_statement_with_psycopg2_api
     expected_result = "CREATE TABLE temp_testing (cover text)"
 
-
     with mocker.patch.object(psycopg2, 'connect') as mock_connection:
         pysoni_client.postgre_statement(
             "CREATE TABLE temp_testing (cover text)")

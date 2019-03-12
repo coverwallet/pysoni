e2deb388c2ad69aa8bc5952512a80cd82199a49c
Javier162380 <javierllorente16@gmail.com>
2019-03-12 10:50:13 +0100

    upload test to update table

---
+++ tests/core/test_update_table.py
@@ -0,0 +1,22 @@
+import pytest
+
+def test_update_records(pysoni_client, mocker):
+    mocker.patch.object(pysoni_client, "connection")
+    mocker.patch.object(pysoni_client, "postgre_statement")
+
+    expected_statement = """UPDATE table
+                            SET column='2'
+                            WHERE column='1'""",
+                                    
+
+    pysoni_client.update_fields(
+        tablename='table',
+        column='column',
+        values=[['1','2']],
+        wait_time=0
+    )
+
+    pysoni_client.postgre_statement.assert_called_once_with(
+        expected_statement,
+        timesleep=0
+    )

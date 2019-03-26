151a1be612de70d96cf62554d119d5d6b045b582
Alex Gascón <alex.gascon@coverwallet.com>
2019-03-26 12:05:06 +0100

    Adding class docstring + minor improvements in method docstrings

--- pysoni/connection.py
+++ pysoni/connection.py
@@ -3,8 +3,37 @@ from psycopg2.extensions import parse_dsn
 
 
 class Connection:
+    """Class to represent database connecions
+
+    Instances of this class will contain the logic to do the operations related
+    specifically with the DB connection: opening/closing, obtaining cursors,
+    commiting changes...
+    """
+
     def __init__(self, port=None, host=None, dbname=None, user=None, password=None, uri=None,
                  connection_options=None):
+        """
+        Parameters
+        -----------
+            port : int
+                Port of the host in which the DB is exposed
+            host : str
+                Address of the host in which the DB is running
+            dbname : str
+                Name of our DB
+            user : str
+                Username to use to identify in the DB
+            password : str
+                Password to use to identify in the DB
+            uri : str
+                URI of the DB we want to connect to. If it's included, the DB
+                connection arguments will be parsed from it and the rest of the
+                arguments will be ignored.
+            connecion_options : str
+                Additional options we may want to specify when connecting to
+                the DB. Its value will be sent directly to psycopg2.connect
+        """
+
         if uri:
             uri_dict = parse_dsn(uri)
             
@@ -36,6 +65,7 @@ class Connection:
         Please note that this field is intended to be for internal use only, 
         and that you shouldn't access it directly from your app.
         """
+
         self._connection_handler = psycopg2.connect(**self._build_connection_arguments())
 
     def close(self):
@@ -44,15 +74,22 @@ class Connection:
         Delegates the `close()` method to the psycopg2 connection handler and
         then sets the `_connection_handler` field to None
         """
+
         self._connection_handler.close()
         self._connection_handler = None
         
     def cursor(self):
-        """Obtain a psycopg2 DB cursor"""
+        """Obtain a psycopg2 DB cursor
+
+        Delegates the call to the psycopg2 connection"""
+
         return self._connection_handler.cursor()
 
     def commit(self):
-        """Commit all the changes pending on this connection to the DB"""
+        """Commit all the changes pending on this connection to the DB
+
+        Delegates the call to the psycopg2 connection"""
+
         return self._connection_handler.commit()
 
     def _build_connection_arguments(self):

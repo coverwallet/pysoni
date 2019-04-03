5ab3d4ad4b0bd89a928afd905e00fb7f92f2b973
SergioEG <sergioelola@hotmail.com>
2019-04-03 10:56:10 +0200

    Initial commit. Creating folder 'examples'

---
+++ examples/truncate_all_tables_of_schema.py
@@ -0,0 +1,12 @@
+
+
+import sys
+sys.path.append('..')
+from pysoni import Postgre
+
+""" client = return Postgre(port='5432', host='localhost', 
+    user=os.environ['POSTGRES_USER'], dbname=os.environ['POSTGRES_DB'],
+    password='', connection_options='-c statement_timeout=30000') """
+
+
+

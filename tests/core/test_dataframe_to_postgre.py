482b016a009c7a4c1aeb5412af9218912d5b592b
ramonmarco <juan.ramon.marco@coverwallet.com>
2022-01-13 13:33:23 +0100

    DE-1047 testing None nan behaviour

--- tests/core/test_dataframe_to_postgre.py
+++ tests/core/test_dataframe_to_postgre.py
@@ -52,7 +52,7 @@ def test_dataframe_to_postgre_append_nan(pysoni_client_connection_with_envvars):
         "TRUNCATE TABLE test_dataframe_to_postgre_append;")
 
     print(results.get('results')[1][2])
-    assert (results.get('results')[1][2]).isnan(np.nan)
+    assert (results.get('results')[1][2]).isnull()
 
 
 def test_dataframe_to_postgre_rebuild(pysoni_client_connection_with_envvars):
